cf8c13687f788ea05d00e0c43ca5eb25ba2745bf
Danik <95441554+Unttld00@users.noreply.github.com>
2024-04-18 09:31:58 +0300

    .

--- Учебная практика C#/Приложение А/Практическая часть/Практическая часть задание 1/Задание 1 практическая часть/obj/Debug/net6.0/Задание 1 практическая часть.AssemblyInfo.cs
+++ Учебная практика C#/Приложение А/Практическая часть/Практическая часть задание 1/Задание 1 практическая часть/obj/Debug/net6.0/Задание 1 практическая часть.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Задание 1 практическая часть")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e36fa6c247cb0da1f3b517b0708f0d01fd48449")]
 [assembly: System.Reflection.AssemblyProductAttribute("Задание 1 практическая часть")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Задание 1 практическая часть")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
