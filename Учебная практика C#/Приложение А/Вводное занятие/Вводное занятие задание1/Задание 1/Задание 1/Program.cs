d8bab4fc9a2f026a8e9a20d78aa49a2185fe50cc
Danik <95441554+Unttld00@users.noreply.github.com>
2024-04-18 11:28:40 +0300

    .

--- Учебная практика C#/Приложение А/Вводное занятие/Вводное занятие задание1/Задание 1/Задание 1/Program.cs
+++ Учебная практика C#/Приложение А/Вводное занятие/Вводное занятие задание1/Задание 1/Задание 1/Program.cs
@@ -10,8 +10,8 @@
         Console.WriteLine("Потребляемое автомобилем колво бензина на 100 км: ");
         int CarConsupmption = int.Parse(Console.ReadLine());
         Console.WriteLine("Цена литра бензина: ");
-        double FuelPricePerLitr = int.Parse(Console.ReadLine());
-        double DrivePrice = (DachaDistance / CarConsupmption) * FuelPricePerLitr;
+        double FuelPricePerLitr =Convert.ToDouble(Console.ReadLine());
+        double DrivePrice = (DachaDistance / 100) * CarConsupmption * FuelPricePerLitr;
         Console.WriteLine("Стоимость поездки равна " + DrivePrice);
     }
 
--- Учебная практика C#/Приложение А/Практическая часть/Практическая часть задание3/task3/obj/Debug/net6.0/task3.AssemblyInfo.cs
+++ Учебная практика C#/Приложение А/Практическая часть/Практическая часть задание3/task3/obj/Debug/net6.0/task3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf8c13687f788ea05d00e0c43ca5eb25ba2745bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
